3941ebc4b8c7d40edfad1ff3496c497e1e9de16c
Nathan Santos <nathanaelsantos15@gmail.com>
2024-07-18 03:38:25 -0300

    feat(customer): add update and get all customers functionality in CustomerService

--- services/customer/src/main/java/com/ecommerce/customer/customer/CustomerService.java
+++ services/customer/src/main/java/com/ecommerce/customer/customer/CustomerService.java
@@ -1,7 +1,11 @@
 package com.ecommerce.customer.customer;
 
 import lombok.RequiredArgsConstructor;
+import org.apache.commons.lang.StringUtils;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
@@ -13,5 +17,34 @@ public class CustomerService {
     public String createCustomer(CustomerRequest customer) {
         var request = customerRepository.save(customerMapper.toCustomer(customer));
         return request.getId();
+    }
+
+    public void updateCustomer(CustomerRequest request) {
+        var customer = customerRepository.findById(request.id())
+                .orElseThrow(() -> new CustomerNotFoundException("Customer not found"));
+
+        mergeCustomer(customer, request);
+        customerRepository.save(customer);
+    }
+
+    private void mergeCustomer(Customer customer, CustomerRequest request) {
+        if (StringUtils.isNotBlank(request.firstName()))
+            customer.setFirstName(request.firstName());
+
+        if (StringUtils.isNotBlank(request.lastName()))
+            customer.setLastName(request.lastName());
+
+        if (StringUtils.isNotBlank(request.email()))
+            customer.setEmail(request.email());
+
+        if (request.address() != null)
+            customer.setAddress(request.address());
+    }
+
+    public List<CustomerResponse> getALlCustomers() {
+        return customerRepository.findAll()
+                .stream()
+                .map(CustomerMapper::fromCustomer)
+                .collect(Collectors.toList());
     }
 }
